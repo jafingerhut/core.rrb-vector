1ba40768a041c2751aa308da134698f5c09203d7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-08-05 13:13:21 -0700

    Change how assert-vector-like is invoked within deftest
    assert-vector-like returns nil when it succeeds.  It is not supposed
    to be wrapped inside of an invocation of `clojure.test/is`.  If you do
    that, you get a test failure when the testing finds no problems.

--- src/test_local/clojure/clojure/core/rrb_vector_check.clj
+++ src/test_local/clojure/clojure/core/rrb_vector_check.clj
@@ -5,6 +5,6 @@
   (:use clojure.test))
 
 (deftest collection-check
-  (is (assert-vector-like 250 (fv/vector) gen/int))
+  (assert-vector-like 250 (fv/vector) gen/int)
   (is (every? nil? (.-array ^clojure.lang.PersistentVector$Node
                             (.-root ^clojure.lang.PersistentVector (vector))))))
