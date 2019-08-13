684d9b5fdc213d7cd4a12f039e0991eb0b404369
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-08-13 01:36:42 -0700

    Remove now-redundant rrb-failing-tests ns, and enable tests to run
    with older versions of Clojure back to 1.7.0.

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.2.0"
   :parent [org.clojure/pom.contrib "0.1.2"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :source-paths ["src/main/clojure"
-                 "src/main/cljc"
-                 "src/main/cljs"]
-  :test-paths ["src/test/clojure"]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :source-paths ["src/main/clojure" "src/main/cljs" "src/main/cljc"]
+  :test-paths ["src/test/clojure" "src/test/cljs" "src/test/cljc"]
   :jvm-opts ^:replace ["-XX:+UseG1GC"
                        "-XX:-OmitStackTraceInFastThrow"]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]
@@ -21,10 +19,8 @@
                                     [org.clojure/clojure "1.7.0"]
                                     [org.clojure/test.check "0.9.0"]]
                      :test-paths ["src/test_local/clojure"]}
-             :test-failing {:test-paths ["src/test_failing/clojure"]}
              :cljs {:dependencies [[org.clojure/clojure "1.10.0"]
                                    [org.clojure/clojurescript "1.10.238"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
